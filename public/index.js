4c03a4ac9105a24cf7a643251b72d1cf6c2570d9
sbroken10 <79272375+sbroken10@users.noreply.github.com>
2021-09-15 16:31:56 -0500

    Desafio websocket 2 15/09/2021

--- public/index.js
+++ public/index.js
@@ -6,16 +6,46 @@ document.addEventListener("DOMContentLoaded", function () {
     const img = document.getElementById("thumbnail");
     const button = document.getElementById("send")
     let arrPro = []
+    let chatLogs = []
     let nomVal = ""
     let priVal = ""
     let imgVal = ""
+    let email = ""
+    let message = ""
     const myTemplate = document.getElementById("template")
     const render = document.getElementById("render")
+    const templateChat = document.getElementById("template-chat")
+    const chatRender = document.getElementById("chatRender")
+    const emailInput = document.getElementById("emailImput");
+    const msg = document.getElementById("messageImput");
+    const msgB = document.getElementById("messageButton")
+
+
+
+
     function genID() {
 
         let tempID = arrPro.length + 1;
         return tempID.toString()
     }
+
+    function valEmail(data) {
+        const regex = /^([a-zA-Z0-9_])+\@(([a-zA-Z0-9])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (regex.test(data)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    emailInput.addEventListener("keyup", function (e) {
+        email = e.target.value
+        console.log(email)
+    })
+
+    msg.addEventListener("keyup", function (e) {
+        message = e.target.value
+    })
 
     nombre.addEventListener("keyup", function (e) {
         console.log(e.target.value)
@@ -41,13 +71,22 @@ document.addEventListener("DOMContentLoaded", function () {
         socket.emit("productSave", { title: nomVal, price: priVal, thumbnail: imgVal, id: genID() })
     })
 
+    msgB.addEventListener("click", function () {
+
+        if (valEmail(email)) {
+            let da = new Date();
+            socket.emit("chat", { msg: message, usr: email, date: da.getDate() + "/" + da.getMonth() + "/" + da.getFullYear() + " " + da.getHours() + ":" + da.getMinutes() + ":" + da.getSeconds() })
+        } else {
+            alert("El correo introducido no es valido")
+        }
+    })
+
     socket.on('update', (data) => {
         console.log(data)
         arrPro = data
         console.log(arrPro)
         const template = Handlebars.compile(myTemplate.innerHTML)
         render.innerHTML = template({ itemExist: true, arrPro: arrPro })
-
     })
     socket.on('constant', (data) => {
         console.log(data)
@@ -55,7 +94,13 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(arrPro)
         const template = Handlebars.compile(myTemplate.innerHTML)
         render.innerHTML = template({ itemExist: true, arrPro: arrPro })
-
+    })
+    socket.on('updateMessage', (data) => {
+        console.log(data)
+        chatLogs = data
+        console.log(chatLogs)
+        const chatTemplate = Handlebars.compile(templateChat.innerHTML)
+        chatRender.innerHTML = chatTemplate({ messages: chatLogs })
     })
 })
 
--- server.js
+++ server.js
@@ -12,16 +12,18 @@ let arrPro = [
         id: '2'
     },
 ];
+let msgArr = []
 const express = require('express'),
-      app = express(),
-      http = require('http').Server(app),
-      io = require('socket.io')(http);
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
 
 
 const handlebars = require('express-handlebars')
 const router = require('./routes/routes.js')
 
+const fs = require('fs');
 
 http.listen(8080, () => {
     console.log("servidor en el puerto 8080")
@@ -39,17 +41,70 @@ app.use(express.static('./public'))
 app.use('/api', router)
 app.set('socketio', io)
 app.get('/', (req, res, next) => {
-    res.sendFile('index.html', {root: __dirname} )
+    res.sendFile('index.html', { root: __dirname })
 })
 
+function guardar(message) {
+    try {
+        const data = fs.readFileSync('./messages.json', 'utf-8');
+        console.log(data)
+        const json = JSON.parse(data.toString('utf-8'))
+        console.log(json)
+        console.log(message)
+        json.push({ ...message })
+        console.log(json)
+        try {
+            fs.writeFileSync('./messages.json', JSON.stringify(json, null, '\t'))
+        } catch (err) {
+            throw new Error(err)
+        }
+    } catch (err) {
+        try {
+            console.log('No existe el archivo para agregar los productos, se procede a crearlo')
+            fs.writeFile('./messages.json', JSON.stringify([{ ...message }]))
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+}
+function leer() {
+    try {
+        const data = fs.readFileSync('./messages.json');
+        const json = JSON.parse(data.toString('utf-8'))
+
+        console.log("se va a leer el archivo")
+        console.log(json)
+        msgArr = json
+        console.log(msgArr)
+    } catch (err) {
+        try {
+            console.log(["algo paso"])
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+}
+
+
+
 io.on('connection', (socket) => {
-    console.log(socket.id); 
+    console.log(socket.id);
     socket.emit('constant', arrPro)
-    socket.on('productSave', data =>{
+    leer()
+    socket.emit("updateMessage", msgArr)
+    socket.on('productSave', data => {
         arrPro.push(data)
         console.log(arrPro)
         io.emit("update", arrPro)
     })
-  });
+    socket.on('chat', data => {
+
+        console.log("messages data")
+        guardar(data);
+        leer();
+        io.emit("updateMessage", msgArr)
+    })
+});
 
 
