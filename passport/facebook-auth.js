e428484f5ad45a975aeea160c1b7ff22ef29d446
sbroken10 <79272375+sbroken10@users.noreply.github.com>
2022-01-04 23:02:16 -0500

    prueba facebook login

--- passport/facebook-auth.js
+++ passport/facebook-auth.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id, done) => {
 passport.use('facebook', new facebookStrategy({
     clientID: '237716521803373',
     clientSecret: 'c3e133124aaf0c2a6a1ac0fcd536a04b',
-    callbackURL: '/usuario/home',
+    callbackURL: '/usuario/facebook/callback',
     profileFields: ['id', 'displayName', 'name', 'gender', 'picture.type(large)']
 }, function (token, refreshToken, profile, done) {
     process.nextTick(function () {
